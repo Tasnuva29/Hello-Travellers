26ba63972b00032f04174e482c0f27cdf90808bc
ktoufiquee <59536321+ktoufiquee@users.noreply.github.com>
2022-07-18 22:54:36 +0600

    SignUp Backend: 1

---
+++ Controllers/UserAuthController.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Mvc;
+using Hello_Travellers.Models;
+
+namespace Hello_Travellers.Controllers
+{
+    public class UserAuthController : Controller
+    {
+        // GET
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(User user)
+        {
+            Entities db = new Entities();
+            db.Users.Add(user);
+            db.SaveChanges();
+            db.Dispose();
+            return View(user);
+        }
+    }
+}
