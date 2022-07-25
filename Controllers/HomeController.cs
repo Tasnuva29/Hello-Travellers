126bd196bcf583933991412009610b2e4bed9459
SarkerSamir <rohitsarker5@gmail.com>
2022-07-25 20:53:01 +0600

    Backup

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -24,5 +25,9 @@ namespace Hello_Travellers.Controllers
             ViewBag.Message = "Your contact page.";
             return View();
         }
+
+
+      
+
     }
 }
