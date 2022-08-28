e3be2550f95e8679448411b6d30eedbc9532f612
ktoufiquee <59536321+ktoufiquee@users.noreply.github.com>
2022-08-29 01:59:39 +0600

    Database: Report Entity Updated

--- Models/Report.cs
+++ Models/Report.cs
@@ -19,6 +19,7 @@ namespace Hello_Travellers.Models
         public string ContextID { get; set; }
         public string Reason { get; set; }
         public string ReporterUsername { get; set; }
+        public string Status { get; set; }
     
         public virtual User User { get; set; }
     }
